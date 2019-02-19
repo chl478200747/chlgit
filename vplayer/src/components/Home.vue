808dd024612ba64794b461dcdd6fd1d067d26b08
chl <632031719@qq.com>
2019-02-19 17:54:35 +0800

    'vplayer'

--- vplayer/src/components/Home.vue
+++ vplayer/src/components/Home.vue
@@ -1,19 +1,157 @@
 <template>
-    <div class="home">
-        <Header></Header>
-        <Menu></Menu>
+  <div class="home">
+    <!-- 头部菜单 -->
+    <div class="header">
+      <div class="aside-item pl">
+        <span class="area-label">成都</span>
+        <span class="icon-arrow"></span>
+      </div>
+      <div class="center-item">
+        <input type="text" placeholder="找影视剧、影院、影人">
+      </div>
+      <div class="aside-item pr">
+        <span class="icon-list"></span>
+      </div>
     </div>
+    <!-- 首页内容 -->
+    <div class="main">
+      <div class="tab">
+        <div class="tab-item" :class="{select:tabShow==1}">
+          <div class="tab-item-title">正在热映</div>
+        </div>
+        <div class="tab-item" :class="{select:tabShow==2}">
+          <div class="tab-item-title">即将上映</div>
+        </div>
+      </div>
+    </div>
+    <!-- 底部菜单组件 -->
+    <Menu></Menu>
+  </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
-import Menu from '@/components/Menu'
+import { Tab, TabItem } from "vux";
+import Header from "@/components/Header";
+import Menu from "@/components/Menu";
+import func from "./vue-temp/vue-editor-bridge";
 
 export default {
-    name: 'Home',
-    components: {
-        Header,
-        Menu
+  name: "Home",
+  data() {
+    return {
+      tabShow: 1
+    };
+  },
+  methods: {
+    isHit: function() {
+      this.tabShow = 1;
+    },
+    upComing: function() {
+      this.tabShow = 2;
     }
-}
+  },
+  components: {
+    Tab,
+    TabItem,
+    Menu
+  }
+};
 </script>
+
+<style scope>
+.header {
+  position: relative;
+  width: 100%;
+  height: 65px;
+  background-color: #8cc5d8;
+}
+.aside-item {
+  position: absolute;
+  bottom: 18px;
+}
+.aside-item.pl {
+  left: 18px;
+}
+.aside-item.pr {
+  right: 18px;
+}
+.area-label {
+  float: left;
+  display: block;
+  font-family: "Microsoft YaHei";
+  font-size: 14px;
+  color: #fff;
+}
+.icon-arrow {
+  float: left;
+  display: block;
+  margin-left: 4px;
+  width: 10px;
+  height: 10px;
+  background: url(../assets/images/icon_arrow.png) no-repeat center;
+  background-size: 100%;
+}
+.icon-list {
+  float: left;
+  display: block;
+  width: 27px;
+  height: 14px;
+  background: url(../assets/images/icon_list.png) no-repeat center;
+  background-size: 100%;
+}
+.center-item {
+  position: absolute;
+  left: 50%;
+  bottom: 10px;
+  width: 240px;
+  height: 35px;
+  margin-left: -120px;
+  border-radius: 17px;
+  background-color: #72b5cb;
+}
+.center-item input {
+  width: 205px;
+  height: 100%;
+  margin-left: 17px;
+  text-align: center;
+  border: none;
+  outline: none;
+  background-color: #72b5cb;
+  font-size: 14px;
+  color: #fff;
+}
+input::-webkit-input-placeholder {
+  color: #fff;
+}
+input::-moz-placeholder {
+  color: #fff;
+}
+input::-moz-placeholder {
+  color: #fff;
+}
+/* 导航 */
+.tab {
+  width: 100%;
+  height: 40px;
+  background-color: #b5d5e0;
+}
+.tab-item {
+  float: left;
+  width: 50%;
+  height: 100%;
+  line-height: 32px;
+  padding: 4px;
+  box-sizing: border-box;
+}
+.tab-item-title {
+  width: 100%;
+  height: 100%;
+  color: #262c2e;
+  border-radius: 4px;
+  text-align: center;
+}
+.select .tab-item-title {
+  color: #fff;
+  background-color: #81bbce;
+}
+</style>
--- vplayer/src/components/Menu.vue
+++ vplayer/src/components/Menu.vue
@@ -3,23 +3,25 @@
     <!-- 底部导航菜单 -->
     <tabbar>
       <tabbar-item>
-        <img slot="icon" src="../assets/demo/icon_nav_button.png">
+        <img slot="icon" src="../assets/menu/icon_yingyuan.png">
         <span slot="label">影院</span>
       </tabbar-item>
-      <tabbar-item show-dot>
-        <!-- <img slot="icon" src="../assets/demo/icon_nav_msg.png"> -->
+      <tabbar-item>
+        <img slot="icon" src="../assets/menu/icon_yingquan.png">
         <span slot="label">影圈</span>
       </tabbar-item>
-      <tabbar-item selected link="/component/demo">
-        <!-- <img slot="icon" src="../assets/demo/icon_nav_article.png"> -->
+      <tabbar-item class="tabbar-item-center" link="#">
+        <div slot="icon" class="icon-wrap">
+          <img src="../assets/menu/icon_dianying.png">
+        </div>
         <span slot="label">电影</span>
       </tabbar-item>
-      <tabbar-item badge="2">
-        <!-- <img slot="icon" src="../assets/demo/icon_nav_cell.png"> -->
+      <tabbar-item>
+        <img slot="icon" src="../assets/menu/icon_maipiao.png">
         <span slot="label">卖票</span>
       </tabbar-item>
-      <tabbar-item badge="2">
-        <!-- <img slot="icon" src="../assets/demo/icon_nav_cell.png"> -->
+      <tabbar-item>
+        <img slot="icon" src="../assets/menu/icon_wode.png">
         <span slot="label">我的</span>
       </tabbar-item>
     </tabbar>
@@ -27,16 +29,41 @@
 </template>
 
 <script>
-import {Tabbar, TabbarItem} from 'vux'
+import { Tabbar, TabbarItem } from "vux";
 
 export default {
   name: "menu",
   components: {
-      Tabbar,
-      TabbarItem
+    Tabbar,
+    TabbarItem
   }
 };
 </script>
 
 <style>
+a {
+  text-decoration: none;
+}
+.menu .weui-tabbar {
+  background-color: #8cc5d8 !important;
+}
+.menu .weui-tabbar__label {
+  color: #fff;
+}
+.menu .icon-wrap {
+  width: 50px;
+  height: 50px;
+  background-color: #FFF;
+  border-radius: 50%;
+  margin-top: -25px;
+}
+.menu .icon-wrap img {
+  width: 44px;
+  height: 44px;
+  margin-top: 3px;
+}
+.menu .tabbar-item-center .weui-tabbar__icon {
+  width: auto;
+  height: auto;
+}
 </style>
